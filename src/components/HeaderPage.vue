199a1a3d4c27ac39ab830ee02d57b90585fd985a
Sergio Alferov <alferovsergio@yandex.ru>
2023-02-10 23:01:37 +0300

    text

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,15 +1,5 @@
 <template>
-  <div>
-     <div class="container_nav">
-    <a href="#" class="navbar_brand">Pulse</a>
-    <ul class="navbar_menu">
-        <li><a href="#">Поиск</a></li>
-        <li><a href="#">Чарт</a></li>
-        <li><a href="#">Загрузить</a></li>
-        <li><a href="#">Профиль</a></li>
-    </ul>
-   </div>
-  </div>
+  <div></div>
 </template>
 
 <style>
