3592a8d8f780560849602816e1b5ca87063f0252
Kuznetsova Daria <98562706+TruffleDash@users.noreply.github.com>
2023-03-03 21:33:02 +0300

    404 (#25)

--- client/src/views/404Page.vue
+++ client/src/views/404Page.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
-    
+    <div class="container_404">
+      <div><img src="../assets/images/icons/404.svg" class="num_404" alt="404"></div>
+      <div><img src="../assets/images/icons/heart.svg" class="heart_404" alt="heart_404"></div>
+       <p class="paragraph_404">Упс! Страница не найдена</p>  
+         <img src="../assets/images/icons/pulse_404.svg" class="" alt="404">   
+    </div>
   </div>
 </template>
 
 <style>
-
+.container_404 {
+ margin-top: 80px;
+}
+.paragraph_404{
+font-family: 'Commissioner';
+font-style: normal;
+font-weight: 400;
+color: #FFFFFF;
+}
 </style>
 
 <script>
